65a986a54fa1feed5a1f68889cbfada32e2c13f0
Sharath Prodduturi <sharath.prodduturi@disney.com>
2016-08-03 22:12:53 -0400

    Initial commit.

---
+++ app/src/main/java/com/sharathp/blabber/di/ApplicationComponent.java
@@ -0,0 +1,11 @@
+package com.sharathp.blabber.di;
+
+import com.sharathp.blabber.di.modules.ApplicationModule;
+
+import dagger.Component;
+
+@Component(modules = ApplicationModule.class)
+public interface ApplicationComponent {
+
+
+}
