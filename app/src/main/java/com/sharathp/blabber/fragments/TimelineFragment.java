bf5804e4e9e8476e3cd4e6a247138057f073716b
sharathp <chandra.ps@gmail.com>
2016-08-05 22:53:20 -0400

    TimelineFragment in progress.

--- app/src/main/java/com/sharathp/blabber/fragments/TimelineFragment.java
+++ app/src/main/java/com/sharathp/blabber/fragments/TimelineFragment.java
@@ -1,50 +1,125 @@
 package com.sharathp.blabber.fragments;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.sharathp.blabber.BlabberApplication;
+import com.sharathp.blabber.R;
+import com.sharathp.blabber.databinding.FragmentTimelineBinding;
 import com.sharathp.blabber.models.TweetWithUser;
+import com.sharathp.blabber.repositories.TwitterDAO;
+import com.sharathp.blabber.views.EndlessRecyclerViewScrollListener;
+import com.sharathp.blabber.views.adapters.TweetCallback;
+import com.sharathp.blabber.views.adapters.TweetsAdapter;
 import com.yahoo.squidb.data.SquidCursor;
+import com.yahoo.squidb.sql.Order;
+import com.yahoo.squidb.sql.Query;
 
 import org.greenrobot.eventbus.EventBus;
 
 import javax.inject.Inject;
 
 public class TimelineFragment extends Fragment implements LoaderManager.LoaderCallbacks<SquidCursor<TweetWithUser>> {
-    private static final int FEED_ITEM_LOADER_ID = 0;
+    private static final int TWEET_ITEM_LOADER_ID = 0;
     private static final int VISIBLE_THRESHOLD = 5;
 
     @Inject
     EventBus mEventBus;
 
-    private Callback mCallback;
+    @Inject
+    TwitterDAO mTwitterDAO;
+
+    private TweetCallback mCallback;
+
+    private FragmentTimelineBinding mBinding;
+    private TweetsAdapter mTweetsAdapter;
+    private EndlessRecyclerViewScrollListener mEndlessRecyclerViewScrollListener;
 
     @Override
     public void onAttach(final Context context) {
         super.onAttach(context);
-        if ( !(context instanceof Callback)) {
+        if (!(context instanceof Callback)) {
             throw new IllegalArgumentException("context must implement: " + Callback.class.getName());
         }
-        mCallback = (Callback) context;
-    }
-
-
-    @Override
-    public Loader<SquidCursor<TweetWithUser>> onCreateLoader(int id, Bundle args) {
-        return null;
+        mCallback = (TweetCallback) context;
     }
 
     @Override
-    public void onLoadFinished(Loader<SquidCursor<TweetWithUser>> loader, SquidCursor<TweetWithUser> data) {
+    public void onCreate(@Nullable final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        BlabberApplication.from(getActivity()).getComponent().inject(this);
+    }
 
+    @Nullable
+    @Override
+    public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable final Bundle savedInstanceState) {
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_timeline, container, false);
+        return mBinding.getRoot();
     }
 
     @Override
-    public void onLoaderReset(Loader<SquidCursor<TweetWithUser>> loader) {
+    public void onViewCreated(final View view, @Nullable final Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initViews();
+    }
 
+    @Override
+    public void onActivityCreated(@Nullable final Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getLoaderManager().initLoader(TWEET_ITEM_LOADER_ID, null, this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mEventBus.register(this);
+    }
+
+    @Override
+    public void onStop() {
+        mEventBus.unregister(this);
+        super.onStop();
+    }
+
+    @Override
+    public Loader<SquidCursor<TweetWithUser>> onCreateLoader(final int id, final Bundle args) {
+        final Query query = Query.select(TweetWithUser.PROPERTIES)
+                .orderBy(Order.desc(TweetWithUser.CREATED_AT));
+        return mTwitterDAO.getTweets(query);
+    }
+
+    @Override
+    public void onLoadFinished(final Loader<SquidCursor<TweetWithUser>> loader, final SquidCursor<TweetWithUser> data) {
+        // TODO - show no tweets if no rows are retrieved
+        mTweetsAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(final Loader<SquidCursor<TweetWithUser>> loader) {
+        mTweetsAdapter.swapCursor(null);
+    }
+
+    private void initViews() {
+        mTweetsAdapter = new TweetsAdapter(mCallback);
+        final RecyclerView moviesRecyclerView = mBinding.rvTweets;
+        final RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        moviesRecyclerView.setAdapter(mTweetsAdapter);
+        moviesRecyclerView.setLayoutManager(layoutManager);
+        mBinding.srlTweets.setOnRefreshListener(() -> refreshTweets());
+    }
+
+    private void refreshTweets() {
+        // TODO - implement refreshing tweets
     }
 
     /**
