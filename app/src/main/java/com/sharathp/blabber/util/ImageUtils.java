318f8a5c260b5cb26ca3469bd8d5dd0fdaded2cf
sharathp <chandra.ps@gmail.com>
2016-08-07 01:44:37 -0400

    Modified margin in tweet list screen, using higher res images for profile.

--- app/src/main/java/com/sharathp/blabber/util/ImageUtils.java
+++ app/src/main/java/com/sharathp/blabber/util/ImageUtils.java
@@ -10,15 +10,22 @@ import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
 
 public class ImageUtils {
     private static final int TRANSFORM_RADIUS = 5;
-    private static final int TRANSFORM_MARGIN = 2;
+    private static final int TRANSFORM_MARGIN = 0;
+
+    private static final String DEFAULT_QUALITY = "_normal";
+    private static final String DESIRED_QUALITY = "_bigger";
 
 
     public static void loadImage(final Context context, final ImageView imageView, final String url) {
         Glide.with(context)
-                .load(url)
+                .load(getDesiredQualityImageUrl(url))
                 .bitmapTransform(new RoundedCornersTransformation(context, TRANSFORM_RADIUS, TRANSFORM_MARGIN))
                 .placeholder(R.drawable.ic_progress_indeterminate)
                 .error(R.drawable.ic_error)
                 .into(imageView);
+    }
+
+    private static String getDesiredQualityImageUrl(final String url) {
+        return url.replace(DEFAULT_QUALITY, DESIRED_QUALITY);
     }
 }
